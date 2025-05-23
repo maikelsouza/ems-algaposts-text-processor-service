c156ef0c78facf6a921153b705bf9ad81d585eaa
RE045908 <re045908@qintess.com>
2025-05-23 16:20:57 -0300

    New Project Files

---
+++ src/main/java/com/algaworks/algapost/text/processor/service/TextProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algapost.text.processor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algapost/text/processor/service/TextProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algapost.text.processor.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
